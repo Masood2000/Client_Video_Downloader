f0b10775d3bf7a62c683b340fe73742a87f10532
Masood2000 <masudurrehman2000@gmail.com>
2025-01-13 19:52:13 +0500

    half work done

--- app/build/tmp/kapt3/stubs/debug/com/example/apiproject/data/interfaces/ClickHandler.java
+++ app/build/tmp/kapt3/stubs/debug/com/example/apiproject/data/interfaces/ClickHandler.java
@@ -1,6 +1,6 @@
 package com.example.apiproject.data.interfaces;
 
-@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000\u0010\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0010\u0002\n\u0000\bf\u0018\u00002\u00020\u0001J\b\u0010\u0002\u001a\u00020\u0003H&\u00a8\u0006\u0004"}, d2 = {"Lcom/example/apiproject/data/interfaces/ClickHandler;", "", "onClickPressed", "", "VideoDownloader (3) - 1.3_debug"})
+@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000\u0010\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0010\u0002\n\u0000\bf\u0018\u00002\u00020\u0001J\b\u0010\u0002\u001a\u00020\u0003H&\u00a8\u0006\u0004"}, d2 = {"Lcom/example/apiproject/data/interfaces/ClickHandler;", "", "onClickPressed", "", "VideoDownloader (4) - 1.4_debug"})
 public abstract interface ClickHandler {
     
     public abstract void onClickPressed();
--- app/build/tmp/kapt3/stubs/debug/com/example/apiproject/data/interfaces/DeleteClickInterface.java
+++ app/build/tmp/kapt3/stubs/debug/com/example/apiproject/data/interfaces/DeleteClickInterface.java
@@ -1,6 +1,6 @@
 package com.example.apiproject.data.interfaces;
 
-@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000\u0016\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0010\u0002\n\u0000\n\u0002\u0010\u000e\n\u0000\bf\u0018\u00002\u00020\u0001J\u0010\u0010\u0002\u001a\u00020\u00032\u0006\u0010\u0004\u001a\u00020\u0005H&\u00a8\u0006\u0006"}, d2 = {"Lcom/example/apiproject/data/interfaces/DeleteClickInterface;", "", "deleteDownloadingVideo", "", "path", "", "VideoDownloader (3) - 1.3_debug"})
+@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000\u0016\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0010\u0002\n\u0000\n\u0002\u0010\u000e\n\u0000\bf\u0018\u00002\u00020\u0001J\u0010\u0010\u0002\u001a\u00020\u00032\u0006\u0010\u0004\u001a\u00020\u0005H&\u00a8\u0006\u0006"}, d2 = {"Lcom/example/apiproject/data/interfaces/DeleteClickInterface;", "", "deleteDownloadingVideo", "", "path", "", "VideoDownloader (4) - 1.4_debug"})
 public abstract interface DeleteClickInterface {
     
     public abstract void deleteDownloadingVideo(@org.jetbrains.annotations.NotNull()
