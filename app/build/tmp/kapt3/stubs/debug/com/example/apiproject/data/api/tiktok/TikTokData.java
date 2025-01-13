a39166faaaec84b0ee5cf8ceb78b0092dd1e5064
Masood2000 <masudurrehman2000@gmail.com>
2025-01-13 20:10:10 +0500

    all done .....

--- app/build/tmp/kapt3/stubs/debug/com/example/apiproject/data/api/tiktok/TikTokData.java
+++ app/build/tmp/kapt3/stubs/debug/com/example/apiproject/data/api/tiktok/TikTokData.java
@@ -1,6 +1,6 @@
 package com.example.apiproject.data.api.tiktok;
 
-@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000\u0014\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\u0018\u00002\u0012\u0012\u0004\u0012\u00020\u00020\u0001j\b\u0012\u0004\u0012\u00020\u0002`\u0003B\u0005\u00a2\u0006\u0002\u0010\u0004\u00a8\u0006\u0005"}, d2 = {"Lcom/example/apiproject/data/api/tiktok/TikTokData;", "Ljava/util/ArrayList;", "Lcom/example/apiproject/data/api/tiktok/TikTokDataItem;", "Lkotlin/collections/ArrayList;", "()V", "VideoDownloader (4) - 1.4_debug"})
+@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000\u0014\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\u0018\u00002\u0012\u0012\u0004\u0012\u00020\u00020\u0001j\b\u0012\u0004\u0012\u00020\u0002`\u0003B\u0005\u00a2\u0006\u0002\u0010\u0004\u00a8\u0006\u0005"}, d2 = {"Lcom/example/apiproject/data/api/tiktok/TikTokData;", "Ljava/util/ArrayList;", "Lcom/example/apiproject/data/api/tiktok/TikTokDataItem;", "Lkotlin/collections/ArrayList;", "()V", "VideoDownloader (5) - 1.5_debug"})
 public final class TikTokData extends java.util.ArrayList<com.example.apiproject.data.api.tiktok.TikTokDataItem> {
     
     public TikTokData() {
--- app/build/tmp/kapt3/stubs/debug/com/example/apiproject/data/interfaces/UpdateData.java
+++ app/build/tmp/kapt3/stubs/debug/com/example/apiproject/data/interfaces/UpdateData.java
@@ -1,6 +1,6 @@
 package com.example.apiproject.data.interfaces;
 
-@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000\u0010\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0010\u0002\n\u0000\bf\u0018\u00002\u00020\u0001J\b\u0010\u0002\u001a\u00020\u0003H&\u00a8\u0006\u0004"}, d2 = {"Lcom/example/apiproject/data/interfaces/UpdateData;", "", "update", "", "VideoDownloader (4) - 1.4_debug"})
+@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000\u0010\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0010\u0002\n\u0000\bf\u0018\u00002\u00020\u0001J\b\u0010\u0002\u001a\u00020\u0003H&\u00a8\u0006\u0004"}, d2 = {"Lcom/example/apiproject/data/interfaces/UpdateData;", "", "update", "", "VideoDownloader (5) - 1.5_debug"})
 public abstract interface UpdateData {
     
     public abstract void update();
